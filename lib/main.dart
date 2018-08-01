24c8256ea0c393d1808b3818c28534ba74d84d76
Bigghead <shavenchew@yahoo.com>
2018-08-01 16:08:25 -0700

    forgot to define assets cause we're pros...

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import './pages/homepage.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  debugPaintSizeEnabled = true;
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
