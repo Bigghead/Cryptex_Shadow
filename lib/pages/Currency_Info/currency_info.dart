9c1b6a420390af5924f7e8903ecc5c76c03aa9f3
Bigghead <shavenchew@yahoo.com>
2018-08-04 15:31:11 -0700

    close keyboard on body click

--- lib/pages/Currency_Info/currency_info.dart
+++ lib/pages/Currency_Info/currency_info.dart
@@ -23,41 +23,44 @@ class CurrencyInfo extends StatelessWidget {
     Widget build(BuildContext context) {
       // TODO: implement build
       return Scaffold(
-        body: Column(
-          children: <Widget>[
-            GradientNav( title: 'Cryptex'),
-            GradientBody(
-              child: Container(
-                constraints: BoxConstraints.expand(),
-                  child: ListView(
-                    children: <Widget>[
-                      SizedBox(height: 10.0,),
-                      CurrencyImage(currency['id'], currency['symbol']),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 25.0),
-                        child: CurrencyCard(currency, index),
-                      ),
-                      SizedBox(height: 10.0,),
+        body: GestureDetector(
+          onTap: () { FocusScope.of(context).requestFocus(new FocusNode()); },
+          child: Column(
+            children: <Widget>[
+              GradientNav( title: 'Cryptex'),
+              GradientBody(
+                child: Container(
+                  constraints: BoxConstraints.expand(),
+                    child: ListView(
+                      children: <Widget>[
+                        SizedBox(height: 10.0,),
+                        CurrencyImage(currency['id'], currency['symbol']),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 25.0),
+                          child: CurrencyCard(currency, index),
+                        ),
+                        SizedBox(height: 10.0,),
 
-                      Center( child:InfoText(
-                        title: 'Market Cap', fontSize: 24.0,)
-                      ),
-                      Container(
-                        margin: new EdgeInsets.symmetric(vertical: 8.0),
-                        height: 2.0,
-                        width: MediaQuery.of(context).size.width * .90,
-                        color: Colors.grey
-                      ),
-                      Center( child: InfoText(
-                        title: '\$${formatCurrency.format(double.parse(currency["market_cap_usd"]))}',
-                      ) ),
-                      SizedBox(height: 20.0,),
-                      Converter(currency)
-                    ],
-                  ),
-              ),
-            )
-          ],
+                        Center( child:InfoText(
+                          title: 'Market Cap', fontSize: 24.0,)
+                        ),
+                        Container(
+                          margin: new EdgeInsets.symmetric(vertical: 8.0),
+                          height: 2.0,
+                          width: MediaQuery.of(context).size.width * .90,
+                          color: Colors.grey
+                        ),
+                        Center( child: InfoText(
+                          title: '\$${formatCurrency.format(double.parse(currency["market_cap_usd"]))}',
+                        ) ),
+                        SizedBox(height: 20.0,),
+                        Converter(currency)
+                      ],
+                    ),
+                ),
+              )
+            ],
+          ),
         ),
       );
     }
