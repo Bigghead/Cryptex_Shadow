c010eafbb24fc6ec450fcd131785a318a259ddad
Bigghead <shavenchew@yahoo.com>
2018-08-06 21:05:14 -0700

    sexier new colors

--- lib/UI/gradient_nav.dart
+++ lib/UI/gradient_nav.dart
@@ -24,7 +24,10 @@ class GradientNav extends StatelessWidget {
       ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [Colors.red, Colors.blue],
+            colors: [
+                  Color(int.parse('#642b73'.substring(1, 7), radix: 16) + 0xFF000000),
+                  Color(int.parse('#c6426e'.substring(1, 7), radix: 16) + 0xFF000000)
+            ],
             begin: const FractionalOffset(0.0, 0.0),
             end: const FractionalOffset(0.5, 0.0),
             stops: [0.0, 1.0],
