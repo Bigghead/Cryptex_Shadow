c1871de45ee76c5cb7634d96720fda80dc491a77
Bigghead <shavenchew@yahoo.com>
2018-08-08 12:23:01 -0700

    need to setState for transform to trigger

--- lib/UI/navigation_button.dart
+++ lib/UI/navigation_button.dart
@@ -12,18 +12,33 @@ class _NavButtonState extends State<NavButton> with TickerProviderStateMixin {
 
   bool isOpened = false;
   Animation<double> _animation;
+  Animation<double> _translate;
   AnimationController _controller;
 
   @override
     void initState() {
       // TODO: implement initState
-      super.initState();
 
       _controller = AnimationController(
         duration: const Duration(milliseconds: 1000), vsync: this
-      );
+      )..addListener(() {
+            setState(() {});
+      });
 
       _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+      _translate = Tween<double>(
+        begin: 55.0,
+        end: -14.0,
+      ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.0,
+          0.75,
+          curve: Curves.easeOut,
+        ),
+      ));
+      super.initState();
+      
     }
   
   animate() {
@@ -36,15 +51,60 @@ class _NavButtonState extends State<NavButton> with TickerProviderStateMixin {
   }
 
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return FloatingActionButton(
+    void dispose() {
+      // TODO: implement dispose
+      _controller.dispose();
+      super.dispose();
+    }
+
+  Widget news() {
+    return Container(
+      child: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.new_releases),
+      ),
+    );
+  }
+
+  Widget mainToggle() {
+    return Container(
+      child: FloatingActionButton(
         backgroundColor: Colors.blue,
         onPressed: animate,
         child: AnimatedIcon(
           icon: AnimatedIcons.menu_close,
           progress: _animation
         ),
+      ),
+    );
+  }
+
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Transform(
+            transform: Matrix4.translationValues(
+              0.0,
+              _translate.value * 2.0,
+              0.0,
+            ),
+            child: news(),
+          ),
+          Transform(
+            transform: Matrix4.translationValues(
+              0.0,
+              _translate.value,
+              0.0,
+            ),
+            child: news(),
+          ),
+          mainToggle()
+        ],
       );
     }
 }
